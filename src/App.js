68d91c7a53ae3466b47336169723b850ad666b0d
Wavey <danieloagyapong@gmail.com>
2022-06-18 15:20:58 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,89 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+   
+<div class="container">
+      <div class="row">
+         <div class="col-md-12 grey"><p>0</p></div>
+      </div>
+
+   <div class="row">
+      <div class="col-md-3 light">
+         <p>AC</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>+/-</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>%</p>
+      </div>
+      <div class="col-md-3 orange">
+         <p>÷</p>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-md-3 light">
+         <p>7</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>8</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>9</p>
+      </div>
+      <div class="col-md-3 orange">
+         <p>×</p>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-md-3 light">
+         <p>4</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>5</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>6</p>
+      </div>
+      <div class="col-md-3 orange">
+         <p>-</p>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-md-3 light">
+         <p>1</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>2</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>3</p>
+      </div>
+      <div class="col-md-3 orange">
+         <p>+</p>
+      </div>
+   </div>
+
+   <div class="row">
+      <div class="col-md-6 light">
+         <p>0</p>
+      </div>
+      <div class="col-md-3 light">
+         <p>.</p>
+      </div>
+      <div class="col-md-3 orange">
+         <p>=</p>
+   </div>
+   </div>
+   </div>
+
+    
+    
+  )
 }
 
 export default App;
