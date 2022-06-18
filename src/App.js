2ef0e572fb62db9b73c55d9915503a2c6e6240cc
Wavey <danieloagyapong@gmail.com>
2022-06-18 15:29:52 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -4,79 +4,79 @@ import './App.css';
 function App() {
   return ( 
    
-<div class="container">
-      <div class="row">
-         <div class="col-md-12 grey"><p>0</p></div>
+<div className="container">
+      <div className="row">
+         <div className="col-md-12 grey"><p>0</p></div>
       </div>
 
-   <div class="row">
-      <div class="col-md-3 light">
+   <div className="row">
+      <div className="col-md-3 light">
          <p>AC</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>+/-</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>%</p>
       </div>
-      <div class="col-md-3 orange">
+      <div className="col-md-3 orange">
          <p>÷</p>
       </div>
    </div>
 
-   <div class="row">
-      <div class="col-md-3 light">
+   <div className="row">
+      <div className="col-md-3 light">
          <p>7</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>8</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>9</p>
       </div>
-      <div class="col-md-3 orange">
+      <div className="col-md-3 orange">
          <p>×</p>
       </div>
    </div>
 
-   <div class="row">
-      <div class="col-md-3 light">
+   <div className="row">
+      <div className="col-md-3 light">
          <p>4</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>5</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>6</p>
       </div>
-      <div class="col-md-3 orange">
+      <div className="col-md-3 orange">
          <p>-</p>
       </div>
    </div>
 
-   <div class="row">
-      <div class="col-md-3 light">
+   <div className="row">
+      <div className="col-md-3 light">
          <p>1</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>2</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>3</p>
       </div>
-      <div class="col-md-3 orange">
+      <div className="col-md-3 orange">
          <p>+</p>
       </div>
    </div>
 
-   <div class="row">
-      <div class="col-md-6 light">
+   <div className="row">
+      <div className="col-md-6 light">
          <p>0</p>
       </div>
-      <div class="col-md-3 light">
+      <div className="col-md-3 light">
          <p>.</p>
       </div>
-      <div class="col-md-3 orange">
+      <div className="col-md-3 orange">
          <p>=</p>
    </div>
    </div>
